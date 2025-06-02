6fb009b2b18fb0ec4a357ce5613cbc3f7cc7f695
Jonas Krohn <jonas.krohn@gmx.de>
2025-06-02 10:34:54 +0200

    new layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,25 +26,50 @@ function App() {
   };
 
   return (
-    <>
-      <div className="bg-white shadow-lg rounded-xl p-6 w-full max-w-3xl">
-        <FileUpload
-          label="Protein Data"
-          onFileChange={(f) => {
-            handleFileChange(f, setProteinData);
-          }}
-        />
-        <FileUpload
-          label="Peptite Data"
-          onFileChange={(f) => {
-            handleFileChange(f, setPeptideData);
-          }}
-        />
-
-        <ViolinePlot samples={sampleData} />
-        <Heatmap sample={sampleData} />
+    <div className="w-full flex flex-col lg:flex-row h-screen">
+      {/* <!-- Sidebar/Form --> */}
+      <div className="lg:w-1/4 w-full p-6">
+        <h2 className="text-xl font-semibold mb-4">Form</h2>
+        <form className="space-y-4">
+          <FileUpload
+            label="Protein Data"
+            onFileChange={(f) => {
+              handleFileChange(f, setProteinData);
+            }}
+          />
+          <FileUpload
+            label="Peptite Data"
+            onFileChange={(f) => {
+              handleFileChange(f, setPeptideData);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Name"
+            className="w-full p-2 border rounded"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full p-2 border rounded"
+          />
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">
+            Submit
+          </button>
+        </form>
       </div>
-    </>
+
+      {/* <!-- Main Content/Plots --> */}
+      <div className="lg:w-full p-6 h-screen overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4">Plots</h2>
+        <div className="flex items-center justify-center">
+          <ViolinePlot samples={sampleData} useLogScale={false} />
+        </div>
+        <div className="flex items-center justify-center">
+          <Heatmap sample={sampleData} useLogScale={false} />
+        </div>
+      </div>
+    </div>
   );
 }
 
